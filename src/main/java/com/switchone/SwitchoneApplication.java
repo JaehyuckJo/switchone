c184e9de4ed03ddbf68ae5f459ca195dc9dc8736
JaehyuckJo <cjswo0202@gmail.com>
2026-04-23 16:04:59 +0900

    init project

---
+++ src/main/java/com/switchone/SwitchoneApplication.java
@@ -0,0 +1,13 @@
+package com.switchone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwitchoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwitchoneApplication.class, args);
+    }
+
+}
